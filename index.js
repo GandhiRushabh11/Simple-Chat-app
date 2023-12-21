ee875dc139ebf4fc45f3ae78b3bb1d5b682ff3f8
GandhiRushabh11 <gandhirushabh11@gmail.com>
2023-12-21 23:11:08 +0530

    Added basic socket for user connction

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require("express");
-
+const http = require("http");
 const app = new express();
-
+const { Server } = require("socket.io");
+const path = require("path");
+app.use(express.static(path.resolve("./public")));
 app.get("/", (req, res) => {
-  res.send("Hello From Server");
+  res.sendFile("./public/index.html");
 });
 
-app.listen(8000, () => {
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log(`User Conncted With Socket :${socket.id}`);
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} disconnected`);
+  });
+});
+
+server.listen(8000, () => {
   console.log(`Server listen at 8000`);
 });
